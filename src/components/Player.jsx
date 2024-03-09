5e974e44f287f6ffdd92d504eecdad3daf098ef8
aarchi103 <aarchi0012.be21@chitkara.edu.in>
2024-03-10 01:22:16 +0530

    edited the formatting

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,31 +1,38 @@
 import { useState } from "react";
 
 export default function Player({ initialName, symbol }) {
-    const[playerName, setPlayerName]= useState(initialName);
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
-    setIsEditing((editing)=> !editing);
+    setIsEditing((editing) => !editing);
   }
-  function handleNameChange(event){
+  function handleNameChange(event) {
     setPlayerName(event.target.value);
   }
 
   let editablePlayerName = <span className="player-name">{playerName}</span>;
-//   let buttonCaption='Edit'
+  //   let buttonCaption='Edit'
 
-  if(isEditing){
-    editablePlayerName=<input type="text" required value={playerName} onChange={handleNameChange}/>
+  if (isEditing) {
+    editablePlayerName = (
+      <input
+        type="text"
+        required
+        value={playerName}
+        onChange={handleNameChange}
+      />
+    );
   }
 
   return (
     <>
       <li>
         <span className="player">
-        {editablePlayerName}
+          {editablePlayerName}
           <span className="player-symbol">{symbol}</span>
         </span>
-        <button onClick={handleEditClick}>{isEditing? 'Save':'Edit'}</button>
+        <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
       </li>
     </>
   );
