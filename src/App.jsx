7ddcd8a63ee182079074b210512c138944fb4354
aarchi103 <aarchi0012.be21@chitkara.edu.in>
2024-04-07 00:51:33 +0530

    lifted states up

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import { useState } from 'react'
 
 import Player from './components/Player.jsx'
-import  GameBoard  from './components/GameBoard.jsx'
+import GameBoard  from './components/GameBoard.jsx'
+import Log from './components/Log.jsx';
 function App() {
   const [activePlayer, setActivePlayer]= useState('X');
-  function handleSelectSquareforPlayer(){
+  const [gameTurns, setGameTurns]=useState([]);
+  function handleSelectSquareforPlayer(rowIndex, colIndex){
     setActivePlayer((currActivePlayer)=> currActivePlayer==='X'? 'O':'X');
+    setGameTurns(prevTurns=> {
+      let currentPlayer='X';
+      if(prevTurns.length>0 && prevTurns[0].player==='X'){
+        currentPlayer='O'
+      }
+      const updatedTurns = [
+        {square: {row: rowIndex, col: colIndex}, player:currentPlayer},
+        ...prevTurns
+      ];
+      return updatedTurns;
+    });
   }
 
   return (
@@ -15,10 +28,10 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer==='X'}/>
           <Player initialName="Player 2" symbol="O" isActive={activePlayer==='O'}/>
           </ol>
-          <GameBoard onSelectSquare={handleSelectSquareforPlayer} activePlayerSymbol={activePlayer}/>
+          <GameBoard onSelectSquare={handleSelectSquareforPlayer} turns={gameTurns} />
         </div>
 
-        LOG
+        <Log />
       </main>
   )
 }
