23d4e3e7ea395c3d5312efcedf9e1df7690c2e9d
aarchi103 <aarchi0012.be21@chitkara.edu.in>
2024-04-07 01:01:55 +0530

    generated logs

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
           <GameBoard onSelectSquare={handleSelectSquareforPlayer} turns={gameTurns} />
         </div>
 
-        <Log />
+        <Log turns={gameTurns}/>
       </main>
   )
 }
